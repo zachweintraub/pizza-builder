2f56a35b48b3b563b078876c2fb265a19e163d97
Zach Weintraub <zachweintraub@gmail.com>
2019-04-12 13:35:40 -0700

    add some front end logic

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,6 @@
 //BIZ LOGIC
-function Pizza(size) {
-  this.size = size;
+function Pizza() {
+  this.size = '';
   this.toppings = [];
   this.qty = 1;
 }
@@ -24,12 +24,12 @@ Pizza.prototype.calcPrice = function() {
   if(this.size == 'Small') {
     price += 8;
     for (var i = 0; i < this.toppings.length; i++) {
-      if(this.toppings[i].position == 'whole') {
-        if(this.toppings[i].qty == 'regular') {
+      if(this.toppings[i].position == 'Whole') {
+        if(this.toppings[i].qty == 'Regular') {
           price += 0.5;
         } else price += 1;
       } else {
-        if(this.toppings[i].qty == 'regular') {
+        if(this.toppings[i].qty == 'Regular') {
           price += 0.25;
         } else price += 0.5;
       }
@@ -38,12 +38,12 @@ Pizza.prototype.calcPrice = function() {
   if(this.size == 'Medium') {
     price += 10;
     for (var i = 0; i < this.toppings.length; i++) {
-      if(this.toppings[i].position == 'whole') {
-        if(this.toppings[i].qty == 'regular') {
+      if(this.toppings[i].position == 'Whole') {
+        if(this.toppings[i].qty == 'Regular') {
           price += 0.75;
         } else price += 1.50;
       } else {
-        if(this.toppings[i].qty == 'regular') {
+        if(this.toppings[i].qty == 'Regular') {
           price += 0.40;
         } else price += 0.75;
       }
@@ -52,12 +52,12 @@ Pizza.prototype.calcPrice = function() {
   if(this.size == 'Large') {
     price += 12;
     for (var i = 0; i < this.toppings.length; i++) {
-      if(this.toppings[i].position == 'whole') {
-        if(this.toppings[i].qty == 'regular') {
+      if(this.toppings[i].position == 'Whole') {
+        if(this.toppings[i].qty == 'Regular') {
           price += 1;
         } else price += 2;
       } else {
-        if(this.toppings[i].qty == 'regular') {
+        if(this.toppings[i].qty == 'Regular') {
           price += 0.5;
         } else price += 1;
       }
@@ -66,31 +66,79 @@ Pizza.prototype.calcPrice = function() {
   return price;
 }
 
-Pizza.prototype.compileInfo = function() {
-  var output = [];
-  var wholeToppings = [];
-  var leftToppings = [];
-  var rightToppings = [];
-  output.push(this.qty + ' ' + this.size + ' Pizza');
-  for(var i = 0; i < this.toppings.length; i++) {
-    if(this.toppings[i].position == 'whole') {
-      if(this.toppings[i].qty == 'regular') {
-        wholeToppings.push(this.toppings[i].topping);
-      } else wholeToppings.push('Extra ' + this.toppings[i].topping);
+// Pizza.prototype.compileInfo = function() {
+//   var output = [];
+//   var wholeToppings = [];
+//   var leftToppings = [];
+//   var rightToppings = [];
+//   output.push(this.qty + ' ' + this.size + ' Pizza');
+//   for(var i = 0; i < this.toppings.length; i++) {
+//     if(this.toppings[i].position == 'whole') {
+//       if(this.toppings[i].qty == 'regular') {
+//         wholeToppings.push(this.toppings[i].topping);
+//       } else wholeToppings.push('Extra ' + this.toppings[i].topping);
+//     }
+//     if(this.toppings[i].position == 'left') {
+//       if(this.toppings[i].qty == 'regular') {
+//         leftToppings.push(this.toppings[i].topping);
+//       } else leftToppings.push('Extra ' + this.toppings[i].topping);
+//     }
+//     if(this.toppings[i].position == 'right') {
+//       if(this.toppings[i].qty == 'regular') {
+//         rightToppings.push(this.toppings[i].topping);
+//       } else rightToppings.push('Extra ' + this.toppings[i].topping);
+//     }
+//   }
+//   output.push(wholeToppings);
+//   output.push(leftToppings);
+//   output.push(rightToppings);
+//   return output;
+// }
+
+var currentPizza = new Pizza();
+
+
+function toppingSelect() {
+  currentPizza.toppings = [];
+  $('#toppings-select input:checkbox:checked').each(function(){
+    currentPizza.toppings.push(new Topping($(this).val(), $('#' + ($(this).val()).replace(' ', '-') + '-position').val(), $('#' + $(this).val().replace(' ', '-') + '-qty').val()));
+  });
+  $('#toppings-select input:checkbox:not(:checked)').each(function(){
+    for(var i = 0; i < currentPizza.toppings.length; i++) {
+      if(currentPizza.toppings[i].topping == $(this).val()) {
+        currentPizza.toppings.splice(i, 1);
+      }
     }
-    if(this.toppings[i].position == 'left') {
-      if(this.toppings[i].qty == 'regular') {
-        leftToppings.push(this.toppings[i].topping);
-      } else leftToppings.push('Extra ' + this.toppings[i].topping);
-    }
-    if(this.toppings[i].position == 'right') {
-      if(this.toppings[i].qty == 'regular') {
-        rightToppings.push(this.toppings[i].topping);
-      } else rightToppings.push('Extra ' + this.toppings[i].topping);
+  });
+}
+
+function toppingPositionCustomize(topping, position) {
+  for(var i = 0; i < currentPizza.toppings.length; i++) {
+    if(currentPizza.toppings[i].topping == topping.replace('-', ' ')) {
+      currentPizza.toppings[i].position = position;
     }
   }
-  output.push(wholeToppings);
-  output.push(leftToppings);
-  output.push(rightToppings);
-  return output;
 }
+
+$(function(){
+  $('#size-select input').change(function(){
+    currentPizza.size = $('#size-select input:checked').val();
+    $('#toppings-select').slideDown();
+  });
+
+  $('#toppings-select input:checkbox').change(function(){
+    $(this).siblings('.row').slideToggle();
+    toppingSelect();
+  });
+
+  $('[id$=position]').change(function(){
+    console.log($(this).attr('id').slice(0, -9))
+    toppingPositionCustomize($(this).attr('id').slice(0, -9), $(this).val());
+  });
+
+  $('[id$=qty]').change(function(){
+    console.log('works');
+  });
+
+
+});
